9b8b886375f8a0974ebbfa647b878e91351b5ac1
Glitch (vue-test-app-router) <none>
2020-02-23 21:10:04 +0000

    🌮👂 Updated with Glitch

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <LoadingButton>yo</LoadingButton>
+  </div>
+</template>
+
+<script>
+import LoadingButton from "../components/Button.coffee.vue";
+
+export default {
+  components: {
+    LoadingButton
+  }
+};
+</script>
+<style></style>
